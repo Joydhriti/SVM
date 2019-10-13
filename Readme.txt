6e6cffad780a3530379d5c4465c512d1121fd7c5
Joydhriti <50093987+Joydhriti@users.noreply.github.com>
2019-10-13 16:50:05 +0600

    Add files via upload

---
+++ Readme.txt
@@ -0,0 +1,5 @@
+* dataset was taken from dataworld.com
+* At first in data preprocessing part all the unnecessary columns were terminated.
+* After that Naive Bayes was applied.
+
+** Unnecessary column means column containging 80% same value
---
+++ SVM.R
@@ -0,0 +1,160 @@
+dataset= read.csv("dataset.csv")
+dataset$Registered.Location=NULL
+dataset$G6_Shelter_materials_received.Tarpaulin=NULL
+dataset$G6_Shelter_materials_received.Bamboos=NULL
+dataset$G6_Shelter_materials_received.Rope=NULL
+dataset$G6_Shelter_materials_received.Nothing=NULL
+dataset$G7_Bedding_materials_received.Blanket=NULL
+dataset$G7_Bedding_materials_received.Mats=NULL
+dataset$G7_Bedding_materials_received.Mosquito.nets=NULL
+dataset$G7_Bedding_materials_received.Nothing=NULL
+dataset$G8_Kitchen_set_received.Pots.pans=NULL
+dataset$G8_Kitchen_set_received.Utensils=NULL
+dataset$G8_Kitchen_set_received.Stove=NULL
+dataset$G8_Kitchen_set_received.Cooking.fuel=NULL
+dataset$G8_Kitchen_set_received.Torch=NULL
+dataset$G8_Kitchen_set_received.Nothing=NULL
+dataset$G9_Hygiene_kit_received.Hygeine.Dignity.kit=NULL
+dataset$G9_Hygiene_kit_received.Nothing=NULL
+dataset$G11_Provided_by_Government=NULL
+dataset$G11_Provided_by_Military=NULL
+dataset$G11_Provided_by_UN_or_INGO=NULL
+dataset$G11_Provided_by_Local_NGO=NULL
+dataset$G11_Provided_by_Religious_Institution=NULL
+dataset$G11_Provided_by_Host_Community=NULL
+dataset$G11_Provided_by_Prefer_not_to_answer=NULL
+dataset$ Missing_boys=NULL
+dataset$ Missing_girls=NULL
+dataset$Opportunity_offered_pple_which_Men=NULL
+dataset$ Opportunity_offered_pple_which_Women=NULL
+dataset$Opportunity_offered_pple_which_Children=NULL
+dataset$Opportunity_offered_pple_who=NULL
+dataset$Shelter_needed_assis.Blankets=NULL
+dataset$Shelter_needed_assis.Floor.mats=NULL
+dataset$Shelter_needed_assis.training.in.shelter.construction=NULL
+dataset$Shelter_needed_assis.Nothing.no.more=NULL
+dataset$Shelter_needed_assis.Dont.know =NULL
+dataset$Children_safety_problems_Prefer_not_to_answer= NULL
+dataset$Female_safety_problems_Prefer_not_to_answer = NULL
+dataset$Male_safety_problems_Prefer_not_to_answer= NULL
+dataset$Protection_incident_reporting_Majhee = NULL
+dataset$Protection_incident_reporting_Police_court= NULL
+dataset$Protection_incident_reporting_Healthcare_point = NULL
+dataset$Protection_incident_reporting_Prefer_not_to_answer= NULL
+dataset$Protection_incident_reporting_Dont_know  = NULL
+dataset$Other_water_sources_Nonimproved_unprotected_spring=NULL
+dataset$Other_water_sources_Improved_protected_spring=NULL
+dataset$Other_drinking_water_sources_Dont_know=NULL
+dataset$Other_drinking_water_sources_Nonimproved_unprotected_spring=NULL
+dataset$Other_drinking_water_sources_Nonimproved_unprotected_well=NULL
+dataset$ Other_drinking_water_sources_Improved_protected_spring=NULL
+dataset$N5_Needed.Information.Dont_know=NULL
+dataset$ N5_Needed.Information.Prevent_attack_harassment=NULL
+dataset$ N5_Needed.Information.Get_help_after_attack_harassment=NULL
+dataset$ N5_Needed.Information.Weather=NULL
+dataset$N5_Needed.Information.Finding_missing_people=NULL
+dataset$ N4_Prefered.Language.English=NULL
+dataset$ N4_Prefered.Language.Bengali=NULL
+dataset$ N3_Communicated_with_by.Dont_know=NULL
+dataset$ N3_Communicated_with_by.Prefer_not_to_answer=NULL
+dataset$ N3_Communicated_with_by.Youtube=NULL
+dataset$  N3_Communicated_with_by.Cleaning_Campaign=NULL
+dataset$ N3_Communicated_with_by.Printed_flyers=NULL
+dataset$ N3_Communicated_with_by.Social_media_incl._Facebook=NULL
+dataset$ N3_Communicated_with_by.Sign_boards=NULL
+dataset$ N3_Communicated_with_by.SW_FM_Radio=NULL
+dataset$ N3_Communicated_with_by.Local_government_leaders=NULL
+dataset$ N3_Communicated_with_by.Newspaper=NULL
+dataset$ N3_Communicated_with_by.Television=NULL
+dataset$ Info_from_whom_Dont_know=NULL
+dataset$ Info_from_whom_Prefer_not_to_answer=NULL
+dataset$ Info_from_whom_Speak_to_majhee=NULL
+dataset$ Chn_prevented_from_school_which_Boys=NULL
+dataset$ Chn_prevented_from_school_which_Girls=NULL
+dataset$ Chn_prevented_from_school_which_Children_without_caregivers=NULL
+dataset$ Chn_prevented_from_school_which_Special_Needs=NULL
+dataset$ Chn_prevented_from_school_which=NULL
+dataset$ Opportunity_offered_pple=NULL
+dataset$ Children_without_services=NULL
+dataset$ Missing_children =NULL
+dataset$ Unaccompanied_children=NULL
+dataset$ Movement_Restrictions_Dont_know=NULL
+dataset$ Movement_Restrictions_Prefer_not_to_answer =NULL
+dataset$ Chn_prevented_from_school_which=NULL
+dataset$Income_Source_1_Skilled_profession_or_technical=NULL
+dataset$I3_Prefer_not_to_answer =NULL
+dataset$I3_Increase_in_movement_restrictions=NULL
+dataset$I3_Decrease_in_movement_restrictions =NULL
+dataset$I3_Worse_physical_access =NULL
+dataset$I3_Improved_physical_accessed=NULL
+dataset$I3_Decrease_in_food_prices =NULL
+dataset$Food_Source_Own_kitchen_garden=NULL
+dataset$Food_Source_Fishing =NULL
+dataset$Food_Source_Begging =NULL
+dataset$Latrine_access_problems_Prefer_to_defecate_outdoors =NULL
+dataset$Water_treatment_method_Solar_disinfection  =NULL
+dataset$Water_treatment_method_Household_filters =NULL
+dataset$Water_access_problems_coping_Dont_know =NULL
+dataset$Water_access_problems_Dont_know =NULL
+dataset$Other_water_sources_Dont_know =NULL
+dataset$K8_Dont_know=NULL
+dataset$K4_Dont_know=NULL
+dataset$Breast_milk_substitutes_dist_by_Prefer_not_to_answer=NULL
+dataset$Breast_milk_substitutes_dist_by_Host_Community=NULL
+dataset$Breast_milk_substitutes_dist_by_Religious_Institution=NULL
+dataset$I14_Decrease_of_worsening_livelihood=NULL
+dataset$I14_Inc_improv_livelihood_Pro =NULL
+dataset$I14_Decrease_worsening_food_basics=NULL
+dataset$I14_Inc_improvement_food_other =NULL
+dataset$I14_Decrease_in_movement_restrictions =NULL
+dataset$l14_Increase_in_movement_restrictions  =NULL
+dataset$I14_Decrease_of_labour_opportunities =NULL
+dataset$I14_Increase_of_labour_opportunities  =NULL
+dataset$Income_Source_1_Selling_assest_from_Myanmar  =NULL
+dataset$Income_Source_1_Zakat   =NULL
+dataset$Income_Source_1_Savings   =NULL
+dataset$Income_Source_1_Agricultural_production_sales   =NULL
+dataset$Income_Source_1_Fishing   =NULL
+dataset$Shelter_needed_assis.I.dont.want.to.answer = NULL
+dataset$G4_Where_do_the_majority_of_households_in_your_location_currently_cook=NULL
+
+
+datatree = dataset[,-152:-168]
+nuemaricTree = data.frame(datatree$Block.ID)
+
+
+# Converting the dataset into numerical values
+i=2
+while(i<=275){
+  c=i-1
+  nuemaricTree=cbind(nuemaricTree[,1:c],data.frame(as.numeric(datatree[,i])))
+  i=i+1
+}
+
+# Changing the column name
+o=1
+while(o<=275){
+  colnames(nuemaricTree)[o]=colnames(datatree)[o]
+  o=o+1
+}
+#######################
+nuemaricTree$Block.ID=NULL
+# Spiliting the dataset into test and train set
+library(caTools)
+set.seed(123)
+split= sample.split(nuemaricTree$Severity_O1_1_Cash, SplitRatio = .70)
+training = subset(nuemaricTree, split==TRUE)
+test = subset(nuemaricTree, split== FALSE)
+#######################################################################
+
+
+#...... Support Vector machine......#
+library(e1071)
+classifier_svm = svm(formula = factor(Severity_O1_1_Cash) ~.,
+                     data = training, type = "C-classification",
+                     kernel= "linear")
+pred_svm <- predict(classifier_svm, test, type = "class")
+mean(pred_svm == test$Severity_O1_1_Cash)                    
+table(pred_svm,test$Severity_O1_1_Cash)  
+
+#........ End of SVM..........#
